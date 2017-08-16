d3cc29f9072801851071bcc6ff89437f68c8b304
tobnys <tobnystrom@gmail.com>
2017-08-16 20:58:43 +0200

    automated money generation added, automated stat updates added

--- public/game.js
+++ public/game.js
@@ -5,20 +5,26 @@
                 "characterName": "testCharacterOne",
                 "userID": 12345,
                 "level": 1,
-                "money": 500
+                "money": 500,
+                "workers": 0
             }
         ]
     };
 
+    // ON BUTTON CLICK, INITIATE WORKER
     $(".js-worker-btn").click(function(){
-        MOCK_DATA.userCharacter[0].money = MOCK_DATA.userCharacter[0].money+1;
-        if(MOCK_DATA.userCharacter[0].money > 510){
-            MOCK_DATA.userCharacter[0].level = 2;
-        }
-        updateCurrentStats();
+       initiateWorker();
     });
 
+    // INITIATE A WORKER 
+    function initiateWorker() {
+        MOCK_DATA.userCharacter[0].workers = MOCK_DATA.userCharacter[0].workers+1;
+    }
 
+    function workerMoneyGeneration(){
+        MOCK_DATA.userCharacter[0].money = MOCK_DATA.userCharacter[0].money+MOCK_DATA.userCharacter[0].workers;
+        console.log("Worker money generation")
+    }
 
     // UPDATE STATS
     function updateCurrentStats() {
@@ -27,6 +33,11 @@
         $("#money").text(`Money: ${MOCK_DATA.userCharacter[0].money}`);
     };
     
-    // TICK FUNCTION TO INITIATE BELOW FUNCTION
-    updateCurrentStats();
+    // TICK FUNCTION FOR STAT UPDATES
+    function statUpdateTick() {
+        updateCurrentStats();
+        console.log("Stats updated.");
+    }
+    setInterval(statUpdateTick, 100);
+    setInterval(workerMoneyGeneration, 1000);
 })();
