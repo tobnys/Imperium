c5fd87ce6c52fe23ec44171b31d7ac7c64f7a019
tobnys <tobnystrom@gmail.com>
2017-08-16 20:42:03 +0200

    further improvements to game layout and functions

--- public/game.js
+++ public/game.js
@@ -1,20 +1,32 @@
-console.log("test");
-
 (function () {
     const MOCK_DATA = {
         "userCharacter": [
             {
                 "characterName": "testCharacterOne",
-                "userID": "12345",
-                "level": "1",
-                "money": "500"
+                "userID": 12345,
+                "level": 1,
+                "money": 500
             }
         ]
     };
 
-    function updateCurrentStats() {
-        $("#name").text(`Name: ${userCharacter[0].characterName}`);
-        console.log("Stats updated.");
-    };
+    $(".js-worker-btn").click(function(){
+        MOCK_DATA.userCharacter[0].money = MOCK_DATA.userCharacter[0].money+1;
+        if(MOCK_DATA.userCharacter[0].money > 510){
+            MOCK_DATA.userCharacter[0].level = 2;
+        }
+        updateCurrentStats();
+    });
 
+
+
+    // UPDATE STATS
+    function updateCurrentStats() {
+        $("#name").text(`Name: ${MOCK_DATA.userCharacter[0].characterName}`);
+        $("#level").text(`Level: ${MOCK_DATA.userCharacter[0].level}`);
+        $("#money").text(`Money: ${MOCK_DATA.userCharacter[0].money}`);
+    };
+    
+    // TICK FUNCTION TO INITIATE BELOW FUNCTION
+    updateCurrentStats();
 })();
